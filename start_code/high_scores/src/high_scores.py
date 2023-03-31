14729e1fb8bf19d471d421d3e644c151ac891676
Corrine Sing <corrine.sing@gmail.com>
2023-03-31 10:38:37 +0100

    Finishing high scores task

--- start_code/high_scores/src/high_scores.py
+++ start_code/high_scores/src/high_scores.py
@@ -10,3 +10,7 @@ def personal_best(scores):
 def personal_top_three(scores):
     sorted_scores = sorted(scores, reverse = True)
     return sorted_scores[0:3]
+
+def high_to_low(scores):
+    scores_ordered = sorted(scores, reverse = True)
+    return scores_ordered
--- start_code/high_scores/tests/high_scores_test.py
+++ start_code/high_scores/tests/high_scores_test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.high_scores import latest, personal_best, personal_top_three
+from src.high_scores import latest, personal_best, personal_top_three, high_to_low
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v4.0.0
 
@@ -29,12 +29,30 @@ class HighScoresTest(unittest.TestCase):
         actual_value = personal_top_three(self.scores)
         self.assertEqual(expected_value, actual_value)
 
-
-    # Test ordered from highest tp lowest
+    # Test ordered from highest to lowest
+    def test_highest_to_lowest(self):
+        expected_value = [10, 5, 4, 2, 2]
+        actual_value = high_to_low(self.scores)
+        self.assertEqual(expected_value, actual_value)
 
     # Test top three when there is a tie
+    def test_can_get_top_three_when_there_is_a_tie(self):
+        scores = [2, 4, 10, 2, 5, 5]
+        expected_value = [10, 5, 5]
+        actual_value = personal_top_three(scores)
+        self.assertEqual(expected_value, actual_value)
 
     # Test top three when there are less than three
+    def test_can_get_top_three_when_there_is_less_than_three(self):
+        scores = [10, 3]
+        expected_value = [10, 3]
+        actual_value = personal_top_three(scores)
+        self.assertEqual(expected_value, actual_value)
+
 
     # Test top three when there is only one
-    
+        def test_can_get_top_three_when_there_is_less_than_three(self):
+            scores = [10]
+            expected_value = [10]
+            actual_value = personal_top_three(scores)
+            self.assertEqual(expected_value, actual_value)
